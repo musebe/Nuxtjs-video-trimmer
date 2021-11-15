b0fab1a98c81ea338913465e34f6b278ce717bb4
musebe <emusebe9@gmail.com>
2021-11-15 23:53:24 +0300

    Done cloudinary intergration

---
+++ pages/index.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="bg-white py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24">
+    <div class="relative max-w-xl mx-auto">
+      <svg
+        class="absolute left-full transform translate-x-1/2"
+        width="404"
+        height="404"
+        fill="none"
+        viewBox="0 0 404 404"
+        aria-hidden="true"
+      >
+        <defs>
+          <pattern
+            id="85737c0e-0916-41d7-917f-596dc7edfa27"
+            x="0"
+            y="0"
+            width="20"
+            height="20"
+            patternUnits="userSpaceOnUse"
+          >
+            <rect
+              x="0"
+              y="0"
+              width="4"
+              height="4"
+              class="text-gray-200"
+              fill="currentColor"
+            />
+          </pattern>
+        </defs>
+        <rect
+          width="404"
+          height="404"
+          fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+        />
+      </svg>
+      <svg
+        class="absolute right-full bottom-0 transform -translate-x-1/2"
+        width="404"
+        height="404"
+        fill="none"
+        viewBox="0 0 404 404"
+        aria-hidden="true"
+      >
+        <defs>
+          <pattern
+            id="85737c0e-0916-41d7-917f-596dc7edfa27"
+            x="0"
+            y="0"
+            width="20"
+            height="20"
+            patternUnits="userSpaceOnUse"
+          >
+            <rect
+              x="0"
+              y="0"
+              width="4"
+              height="4"
+              class="text-gray-200"
+              fill="currentColor"
+            />
+          </pattern>
+        </defs>
+        <rect
+          width="404"
+          height="404"
+          fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+        />
+      </svg>
+      <div class="text-center">
+        <h2
+          class="
+            text-3xl
+            font-extrabold
+            tracking-tight
+            text-gray-900
+            sm:text-4xl
+          "
+        >
+          NuxtJs Video Trimmer
+        </h2>
+        <p class="mt-4 text-lg leading-6 text-gray-500">
+          This service enables us to upload videos for trimming. Upload video in
+          the form below to proceed.
+        </p>
+      </div>
+      <trimmer v-if="uploadVideo" :video="uploadVideo" />
+      <upload v-else @uploaded="storeVideo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Upload from "../components/Upload.vue";
+import Trimmer from "../components/Trimmer.vue";
+export default {
+  components: {
+    Upload,
+    Trimmer,
+  },
+  data() {
+    return {
+      uploadVideo: null,
+    };
+  },
+  methods: {
+    storeVideo(video) {
+      this.uploadVideo = video;
+    },
+  },
+};
+</script>
